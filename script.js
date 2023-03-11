24698b5c04281191656d5f7b8c28e5e29842c46a
AprendIgor <107777408+IgorVsFeijo@users.noreply.github.com>
2023-03-11 17:04:42 -0300

    Criei primeira versão

---
+++ script.js
@@ -0,0 +1,44 @@
+let fotos = {
+Isaías: "isaias.png",
+Leozão: "leo.png",
+Maykon: "may.png",
+Gabriel: "gabirel.png",
+Manu: "manu[.png"
+}
+
+function resultado() {
+    let pessoa = document.getElementById("nome").value
+
+    if(pessoa.includes("desconhecido")){
+        document.getElementById("expulsao").style.display = "block"
+        document.getElementById("boasvindas").style = ""
+    }
+
+    else{
+        document.getElementById("expulsao").style = ""
+
+        for(option of document.getElementsByTagName("option")){
+            option.value += "-" + option.innerHTML
+            console.log(option)
+        }
+
+        pessoa = document.getElementById("nome").value
+
+
+        boasvindas = document.querySelector("#boasvindas h2")
+
+        console.log(boasvindas)
+
+        boasvindas.innerHTML = "Bem vind"
+
+        boasvindas.innerHTML += pessoa[0]
+
+        boasvindas.innerHTML += ", " + pessoa.split("-")[1]
+
+        document.getElementById("boasvindas").style.display = "block"
+
+        if (pessoa.split("-")[1] != 'Dani'){
+            document.getElementsByTagName("img")[0].src = "imgs/" + fotos[pessoa.split("-")[1]]
+        }
+    }
+}
